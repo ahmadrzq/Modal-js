fef9ac4ed113c44aea5f2fe0a185942c849f8ff5
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-03-19 14:57:31 +0700

    create script for modal

---
+++ modal.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("btn");
+const modal = document.getElementById("myModal");
+const closeButton = document.getElementById("btn-close");
+
+btn.addEventListener('click', function(){
+    console.log("Button clicked");
+    modal.style.display = "block";
+});
+
+closeButton.addEventListener('click', ()=>{
+    console.log("Button clicked");
+    modal.style.display = "none";
+})
